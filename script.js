ece50bcea5454ec993fd76f044a616ec2d541d21
Pushkar Khare <kharepushkar2804@gmail.com>
2022-10-11 11:24:01 +0530

    js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const email =  document.getElementById("mail");
 const pass =  document.getElementById("pass");
 const cpass =  document.getElementById("cpass");
 const gender =  document.getElementsByClassName("gender");
+var check = 0;
 // let temp = 0;
 
 
@@ -22,27 +23,58 @@ let removeError = function(){
 let success = function(){
     alert("your form is sucessfully submitted");
 }
+const display = function(returnVal){
+    if(returnVal==true){
+        let data = {
+            firstName:fname.value,
+            lastName: lname.value,
+            phoneNum : phone.value,
+            emailId : email.value,
+            password : pass.value,
+            cpassword : cpass.value,
+            branch : check
+        };
+        console.log(data);
+        success();
+    }
+    }
 // success();
 
 // Regex
 let rPhone = /^[7-9]([0-9]){9}$/;
 let rEmail = /^([a-zA-Z0-9\.-_]+)@([a-zA-Z0-9-])+.([a-z]{2,10})(.[a-z]{2,8})?$/;
 let rName = /^([a-zA-Z]{3,20})$/;
-let rPass = /^([a-zA-Z0-9_@]){8,20}$/;
+let rPass = /^([a-zA-Z0-9_@#]){8,20}$/;
 
 
 
 
+let returnVal = true;
 // Validate form
 function validateForm(){
-    let returnVal = true;
     removeError();
-
+    returnVal = true;
+    
+    // checking radio buttons
+    for (let opt of gender){
+        if(opt.checked == true){
+            check = opt.value;
+            inner("error--gender","");
+            returnVal=true;
+            break;   
+        }
+        else{
+            returnVal= false;   
+            inner("error--gender","**please select an option");
+        }
+    }
+    console.log(returnVal,"button");
     // Checking first name
     fname.value=fname.value.trim();
     if(!(rName.test(fname.value))){
         inner("error--fName","**Invalid");
-        returnVal= false;
+        fname.style.border='2px solid red';
+        returnVal= false;   
     }
     
     if(fname.value==""){
@@ -50,7 +82,7 @@ function validateForm(){
         inner("error--fName","**Can't be empty");
         returnVal= false;
     }
-    
+    console.log(returnVal,"fname");
     // Checking last name
     lname.value=lname.value.trim();
     if(!(rName.test(lname.value))){
@@ -64,7 +96,8 @@ function validateForm(){
         returnVal= false;
     }
 
-
+    console.log(returnVal,"lname");
+    
     // Checking mobile number
     phone.value=phone.value.trim();
     if(!(rPhone.test(phone.value))){
@@ -77,6 +110,7 @@ function validateForm(){
         returnVal= false;
     }
     
+    console.log(returnVal,"phone");
     
     // Checking email
     email.value=email.value.trim();
@@ -89,7 +123,8 @@ function validateForm(){
         inner("error--email","**Can't be empty");
         returnVal= false;
     }
-
+    console.log(returnVal,"email");
+    
     
     // Checking pass error
     pass.value=pass.value.trim();
@@ -101,6 +136,7 @@ function validateForm(){
         inner("error--pass","**Can't be empty");
         returnVal= false;
     }
+    console.log(returnVal,"pass");
     
     // checking cpass error
     cpass.value=cpass.value.trim();
@@ -112,23 +148,11 @@ function validateForm(){
         inner("error--cpass","**Can't be empty");
         returnVal= false;
     }
-    
-    // checking radio buttons
-    
-    for (let opt of gender){
-        if(opt.checked == true){
-            inner("error--gender","");
-            break;   
-        }
-        else{
-            returnVal= false;   
-            inner("error--gender","**please select an option");
-        }
-    }
+    console.log(returnVal,"cpass");
     
     
-    // if(returnVal==true)
-    //     success();
-
+    display(returnVal);
+    
+    
     return returnVal;
 }
